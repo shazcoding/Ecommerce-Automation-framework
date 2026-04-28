9df29f10c3c287cf43da5235a8146e6a8e8cc78f
shameel <shameelbca@gmail.com>
2026-04-28 13:00:42 +0530

    initial commit framework

---
+++ src/test/java/tests/LoginTest.java
@@ -0,0 +1,25 @@
+package tests;
+
+//package tests;
+
+import org.testng.annotations.Test;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.chrome.ChromeDriver;
+import io.github.bonigarcia.wdm.WebDriverManager;
+
+public class LoginTest {
+
+@Test
+public void testLogin() {
+
+WebDriverManager.chromedriver().setup();
+
+WebDriver driver = new ChromeDriver();
+
+driver.get("https://www.saucedemo.com");
+
+System.out.println("Website Opened");
+
+driver.quit();
+}
+}
